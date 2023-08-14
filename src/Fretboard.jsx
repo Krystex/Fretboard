14f6e4785110458b67929966b68f9428efaae0bc
Krystex <5840423+Krystex@users.noreply.github.com>
2023-08-14 12:54:10 +0200

    CSS now gets bundled with esbuild

---
+++ src/Fretboard.jsx
@@ -0,0 +1,7 @@
+import React, { useCallback, useState } from "react"
+
+const Fretboard = ({}) => {
+    return <div>Test</div>
+}
+
+export { Fretboard }
--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,25 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react"
 import { createRoot } from 'react-dom/client'
 import "./musictheory.js"
+import { Fretboard } from "./Fretboard.jsx"
+
+new EventSource('/esbuild').addEventListener('change', () => location.reload())
 
 const App = (props) => {
+
     const [count, setCount] = useState(0);
     const increment = useCallback(() => {
-        setCount(count => count + 1);
-    }, [count]);
+        setCount(count => count + 1)
+    }, [count])
 
-    return(<>
-        <h1>{props.message}</h1>
-        <h2>Count: {count}</h2>
-        <button onClick={increment}>Increment</button>
-    </>)
+    return (
+        <>
+            <h1>{props.message}</h1>
+            <h2>Count: {count}</h2>
+            <Fretboard />
+            <button onClick={increment}>Increment</button>
+        </>
+    )
 }
 
 const root = createRoot(document.getElementById("root"))
