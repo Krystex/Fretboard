940a0cc838dfe1a395e08a77acb142f4741e0f35
Krystex <5840423+Krystex@users.noreply.github.com>
2024-01-27 13:07:27 +0100

    Fixed react warnings

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const Navbar = () => (
       <button type="button" className="hidden items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
       <div className="pl-14">
